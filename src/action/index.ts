b5c087389c3700a7a504b01416187fb82ae26e4c
Matt Ma <bigmabig@gmail.com>
2018-05-28 15:14:46 -0700

    add ast-utils logic

--- src/action/index.ts
+++ src/action/index.ts
@@ -12,43 +12,10 @@ import {
 import * as ts from 'typescript';
 
 import { Schema as ActionOptions } from './schema';
+
 import { InsertChange } from '../utils/change';
+import { findNodes } from '../utils/ast-utils';
 import { parseName } from '../utils/parse-name';
-
-/**
- * Find all nodes from the AST in the subtree of node of SyntaxKind kind.
- * @param node
- * @param kind
- * @param max The maximum number of items to return.
- * @return all nodes of kind, or [] if none is found
- */
-export function findNodes(node: ts.Node, kind: ts.SyntaxKind, max = Infinity): ts.Node[] {
-  if (!node || max == 0) {
-    return [];
-  }
-
-  const arr: ts.Node[] = [];
-  if (node.kind === kind) {
-    arr.push(node);
-    max--;
-  }
-  if (max > 0) {
-    for (const child of node.getChildren()) {
-      findNodes(child, kind, max).forEach(node => {
-        if (max > 0) {
-          arr.push(node);
-        }
-        max--;
-      });
-
-      if (max <= 0) {
-        break;
-      }
-    }
-  }
-
-  return arr;
-}
 
 /**
  * Helper for sorting nodes.
@@ -95,6 +62,7 @@ function fileUpdate(options: ActionOptions): Rule {
     const modulePath = `${(<{ path: string }>options).path}`;
     const source = readIntoSourceFile(tree, modulePath);
     const nodes = findNodes(source, ts.SyntaxKind.EndOfFileToken);
+
     const lastItem = nodes.sort(nodesByPosition).pop();
     if (!lastItem) {
       throw new Error();
